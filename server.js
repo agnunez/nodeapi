8562c280b85aee57891629715c38f2b03c0a6f9a
anunez <anunez@anunez-laptop.(none)>
2012-10-11 16:36:02 +0100

    Initial test of ACRelay with nodejs, express and MongoDB

---
+++ server.js
@@ -0,0 +1,23 @@
+
+
+var express = require('express'),
+    relay = require('./routes/relay');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.get('/', relay.login);
+app.get('/relay', relay.findAll);
+app.get('/relay/:id', relay.findById);
+app.post('/relay', relay.addRelay);
+app.put('/relay/:id', relay.updateRelay);
+app.delete('/relay/:id', relay.deleteRelay);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
+
+
